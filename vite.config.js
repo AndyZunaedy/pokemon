4829f92713be19a602a5c0d2ce258f2f354eb700
azunaedy0727 <azunaedy0727@pnm.co.id>
2025-08-12 23:28:29 +0700

    push first to main

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/pokemon/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
